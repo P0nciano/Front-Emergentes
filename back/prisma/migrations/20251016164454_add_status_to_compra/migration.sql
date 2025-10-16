c2243ec23de02514f04400031d6547c0cf514eec
Felipe Ponciano <felipeponciano7@gmail.com>
2025-10-16 20:15:26 -0300

    Delete back directory

--- back/prisma/migrations/20251016164454_add_status_to_compra/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."compras" ADD COLUMN     "status" VARCHAR(20) NOT NULL DEFAULT 'Aguardando';
--- back/prisma/migrations/20251016194651_ajuste_tamanho_produto/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `foto` on the `produtos` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(500)`.
-
-*/
--- AlterTable
-ALTER TABLE "public"."produtos" ALTER COLUMN "descricao" SET DATA TYPE VARCHAR(1000),
-ALTER COLUMN "foto" SET DATA TYPE VARCHAR(500);
